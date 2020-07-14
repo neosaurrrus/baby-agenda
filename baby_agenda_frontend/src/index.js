3d61932867c4ae31341e295df4eb91c090b3becb
neosaurrrus <lukiek@gmail.com>
2020-07-15 00:43:25 +0100

    refactor into activity class

--- baby_agenda_frontend/src/index.js
+++ baby_agenda_frontend/src/index.js
@@ -1,54 +1,71 @@
-function getActivities(){
-    return fetch('http://localhost:3000/activities')
-    .then (res =>res.json())
-}
 
-function renderActivity(activity){
-    const activities_wrapper = document.getElementById("activities-wrapper")
-    const activity_node = document.createElement("div")
-
-    const activity_name_node = document.createElement("h3")
-    const activity_name_node_text = document.createTextNode(activity.name)
-    activity_name_node.appendChild(activity_name_node_text)
-    activity_node.appendChild(activity_name_node)
-
-    const activity_description_node = document.createElement("p")
-    const activity_description_node_text = document.createTextNode(activity.description)
-    activity_description_node.appendChild(activity_description_node_text)
-    activity_node.appendChild(activity_description_node)
-
-    const activity_minimum_age_node = document.createElement("p")
-    const activity_minimum_age_node_text = document.createTextNode(activity.minimum_age)
-    activity_minimum_age_node.appendChild(activity_minimum_age_node_text)
-    activity_node.appendChild(activity_minimum_age_node)
-
-    const activity_minimum_time_taken_node = document.createElement("p")
-    const activity_minimum_time_taken_node_text = document.createTextNode(activity.minimum_time_taken)
-    activity_minimum_time_taken_node.appendChild(activity_minimum_time_taken_node_text)
-    activity_node.appendChild(activity_minimum_time_taken_node)
-
-    const activity_upvotes_node = document.createElement("p")
-    const activity_upvotes_node_text = document.createTextNode(activity.upvotes)
-    activity_upvotes_node.appendChild(activity_upvotes_node_text)
-    activity_node.appendChild(activity_upvotes_node)
-
-    const activity_downvotes_node = document.createElement("p")
-    const activity_downvotes_node_text = document.createTextNode(activity.downvotes)
-    activity_downvotes_node.appendChild(activity_downvotes_node_text)
-    activity_node.appendChild(activity_downvotes_node)
-
-    activities_wrapper.appendChild(activity_node)
+class Fetcher {
+    constructor(url){
+        return fetch(url)
+        .then (res =>res.json())
+    }
 }
 
 
+class Activity {
+    constructor(activity){
+        this.name = activity.name,
+        this.description = activity.description,
+        this.minimum_age = activity.minimum_age,
+        this.minimum_time_taken = activity.minimum_time_taken,
+        this.upvotes = activity.upvotes,
+        this.downvotes = activity.downvotes
+        this.wrapper = document.getElementById("activities-wrapper")
+        this.render()
+    }
 
-//Start of Javascript
+    static build(){
+        const allActivities =  new Fetcher("http://localhost:3000/activities")
+        allActivities.then( activity => {
+            activity.forEach(activity => {
+                const newActivity = new Activity(activity)
+            })
+        })
+    }
+    render(){
+        const activity_node = document.createElement("div")
+        const activity_name_node = document.createElement("h3")
+        const activity_name_node_text = document.createTextNode(this.name)
 
-getActivities().then( activity => {
-    activity.forEach(activity => {
-        renderActivity(activity)
-    })
-})
+        activity_name_node.appendChild(activity_name_node_text)
+        activity_node.appendChild(activity_name_node)
+        const activity_description_node = document.createElement("p")
+        const activity_description_node_text = document.createTextNode(this.description)
+        activity_description_node.appendChild(activity_description_node_text)
+        activity_node.appendChild(activity_description_node)
+
+        const activity_minimum_age_node = document.createElement("p")
+        const activity_minimum_age_node_text = document.createTextNode(this.minimum_age)
+        activity_minimum_age_node.appendChild(activity_minimum_age_node_text)
+        activity_node.appendChild(activity_minimum_age_node)
+
+        const activity_minimum_time_taken_node = document.createElement("p")
+        const activity_minimum_time_taken_node_text = document.createTextNode(this.minimum_time_taken)
+        activity_minimum_time_taken_node.appendChild(activity_minimum_time_taken_node_text)
+        activity_node.appendChild(activity_minimum_time_taken_node)
+
+        const activity_upvotes_node = document.createElement("p")
+        const activity_upvotes_node_text = document.createTextNode(this.upvotes)
+        activity_upvotes_node.appendChild(activity_upvotes_node_text)
+        activity_node.appendChild(activity_upvotes_node)
+
+        const activity_downvotes_node = document.createElement("p")
+        const activity_downvotes_node_text = document.createTextNode(this.downvotes)
+        activity_downvotes_node.appendChild(activity_downvotes_node_text)
+        activity_node.appendChild(activity_downvotes_node)
+
+        this.wrapper.appendChild(activity_node)
+
+    }
+    
+}
 
 
+
+Activity.build()
 
