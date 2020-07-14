a900e5a62c94b5e9332c313658ffa6e0bd87cd08
neosaurrrus <lukiek@gmail.com>
2020-07-14 22:53:19 +0100

    change migration for age and time take to add minimum

--- baby_agenda_backend/db/migrate/20200714212847_create_activities.rb
+++ baby_agenda_backend/db/migrate/20200714212847_create_activities.rb
@@ -3,8 +3,8 @@ class CreateActivities < ActiveRecord::Migration[6.0]
     create_table :activities do |t|
       t.string :name
       t.string :description
-      t.string :age
-      t.string :time_taken
+      t.string :minimum_age
+      t.string :minimum_time_taken
       t.integer :upvotes
       t.integer :downvotes
 
