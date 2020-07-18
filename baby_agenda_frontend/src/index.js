a799538991b4d9bf416c26deae070ef5390ce090
neosaurrrus <lukiek@gmail.com>
2020-07-19 00:35:28 +0100

    build acdd activity button and corresponding form

--- baby_agenda_frontend/src/index.js
+++ baby_agenda_frontend/src/index.js
@@ -18,11 +18,6 @@ class Activity {
         this.downvotes = activity.downvotes
         this.render()
     }
-
-    static wrapper(){
-        return document.getElementById("activities-wrapper")
-    }
-
     static index(){
         const allActivities =  new Fetcher("http://localhost:3000/activities")
         allActivities.then( activity => {
@@ -53,7 +48,7 @@ class Activity {
         const activity_details_node_text = document.createTextNode("Details")
         activity_details_node.appendChild(activity_details_node_text)
         activity_node.appendChild(activity_details_node)
-        Activity.wrapper().appendChild(activity_node)
+        document.getElementById("activities-wrapper").appendChild(activity_node)
         this.setDetailsEvent()
     }
     
@@ -70,7 +65,7 @@ class ActivityShow {
                 ActivityShow.render(a)
             })
     }
-    
+    //need to add remove event listeners to stop the 2 activites bug
     static showCloseEvent(){
     
         const showCloseButton = document.getElementById(`show-close-button`)
@@ -85,7 +80,7 @@ class ActivityShow {
 
     static render(a){
         const activity_node = document.createElement("div")
-        activity_node.setAttribute(`id`,`activity-show`)
+        activity_node.setAttribute(`id`,`activity-splash`)
         const activity_name_node = document.createElement("h3")
         const activity_name_node_text = document.createTextNode(a.name)
         activity_name_node.appendChild(activity_name_node_text)
@@ -122,15 +117,109 @@ class ActivityShow {
         activity_node.appendChild(activity_details_node)
         document.getElementById("activities-wrapper").appendChild(activity_node)
         ActivityShow.showCloseEvent()
-        
-
-        
+    
     }
 
+} //end of ActivityShow
 
 
+class NewActivity {
+    constructor(){
+        
+        this.renderNewActivityButton()
+        this.newActivityButtonEvent()
+    }
+
+    newActivityButtonEvent(){
+        const showCloseButton = document.getElementById(`add-activity-button`)
+        showCloseButton.addEventListener("click", (e) => {
+            this.renderNewActivityForm()
+        })
+    }
+
+    renderNewActivityForm(){
+        //splash
+        const add_activity_node = document.createElement("div")
+        add_activity_node.setAttribute(`id`,`activity-splash`)
+        //title
+        const add_activity_title_node = document.createElement("h3")
+        const add_activity_title_node_text = document.createTextNode("Add a New Activity")
+        add_activity_title_node.appendChild(add_activity_title_node_text)
+        //actual form
+        const addActivityForm = document.createElement("div")
+        addActivityForm.innerHTML = `
+            <form id="add_activity_form"> 
+            <label for="add_activity_name">Name:</label>
+            <input type="text" id="add_activity_name" placeholder="Enter a short name for the activity">
+            <label for="add_activity_description">Description:</label>
+            <input type="text" id="add_activity_description" placeholder="Describe the activity">
+            <label for="add_activity_age">Minimum Recommended Age:</label>
+            <select name="add_activity_age" id="add_activity_age">
+                <option value="under6">Less than 6 Months</option>
+                <option value="6-12">6-12 Months</option>
+                <option value="12-24">12-24 Months</option>
+                <option value="over24">Over 24 Months</option>
+            </select>
+        
+            <select name="add_activity_time" id="add_activity_time">
+                <option value="under10">Less than 10 Minutes</option>
+                <option value="10-30">10-30 Minutes</option>
+                <option value="30-60">30-60 Minutes</option>
+                <option value="over60">Over 60 Minutes</option>
+            </select>
+            <input type="submit" id="add-activity-submit">
+            </form>
+        `
+        
+        const add_activity_close_node = document.createElement("button")
+        add_activity_close_node.setAttribute(`id`,`add-cancel-button`)
+        const add_activity_close_node_text = document.createTextNode("Cancel")
+        add_activity_close_node.appendChild(add_activity_close_node_text)
+
+        add_activity_node.appendChild(add_activity_title_node)
+        add_activity_node.appendChild(addActivityForm)
+        add_activity_node.appendChild(add_activity_close_node)
+        
+        
+        document.getElementById("activities-wrapper").appendChild(add_activity_node)
+        this.addCancelEvent()
+        this.addSubmitEvent()
+    }
+
+    renderNewActivityButton(){
+        const add_activity_node = document.createElement("button")
+        add_activity_node.setAttribute(`id`,`add-activity-button`)
+        const add_activity_node_text = document.createTextNode("New Activity")
+        add_activity_node.appendChild(add_activity_node_text)
+        document.querySelector("nav").appendChild(add_activity_node)
+    }
+
+    addCancelEvent(){
+        const button = document.getElementById(`add-cancel-button`)
+        button.addEventListener("click", (e) => {
+            document.getElementById(`activity-splash`).remove()
+        })
+    }
+    addSubmitEvent(e){
+        console.log(document.getElementById('add-activity-submit'))
+        const button = document.getElementById(`add-activity-submit`)
+  
+        button.addEventListener("click", (e) => {
+            e.preventDefault()
+            this.submitNewActivity(e)
+        })
+    }
+    submitNewActivity(e){
+        console.log(e)
+    }
 }
 
 
+//Things that run (maybe all in an init afterwards)
+new NewActivity()
 Activity.index()
+//something to set add activity button
+
+//something to build agenda
+
 
