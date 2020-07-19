a125a61989301e48323fd98663ff8908790d9abe
neosaurrrus <lukiek@gmail.com>
2020-07-19 23:30:59 +0100

    add create adn edit routes to respective classes

--- baby_agenda_backend/app/controllers/activities_controller.rb
+++ baby_agenda_backend/app/controllers/activities_controller.rb
@@ -1,4 +1,5 @@
 class ActivitiesController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def index
         activities = Activity.all
         render json: activities 
@@ -7,5 +8,23 @@ class ActivitiesController < ApplicationController
     def show
         activity = Activity.find(params[:id])
         render json: activity
+    end
+
+    def create 
+        activity = Activity.create(activity_params)
+        render json: activity
+    end
+
+    def update
+        activity = Activity.find(params[:id])
+        activity.update(activity_params)
+        render json: activity
+    end
+
+
+    private
+
+    def activity_params
+        params.require(:activity).permit(:name, :description, :minimum_age, :minimum_time_taken)
     end
 end
--- baby_agenda_backend/db/migrate/20200714212847_create_activities.rb
+++ baby_agenda_backend/db/migrate/20200714212847_create_activities.rb
@@ -5,8 +5,8 @@ class CreateActivities < ActiveRecord::Migration[6.0]
       t.string :description
       t.string :minimum_age
       t.string :minimum_time_taken
-      t.integer :upvotes
-      t.integer :downvotes
+      t.integer :upvotes, :default => 0
+      t.integer :downvotes, :default => 0
 
       t.timestamps
     end
