64bccba416515c155220b25f55b3c4b2f8c15d26
neosaurrrus <lukiek@gmail.com>
2020-07-26 12:28:11 +0100

    1st attempt with basic user authentication

--- baby_agenda_backend/app/controllers/application_controller.rb
+++ baby_agenda_backend/app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
+  
+before_action :set_user
+   
     
+    def check_if_belongs_to_user(instance)
+        instance.user_id == current_user.id
+    end
+
+    def current_user
+        user = "none"
+        if session[:id].present?
+            user = User.find(session[:id]) 
+        end
+        user
+        
+    end
+    private
+
+    def set_user
+        cookies[:username] = current_user || 'Guest'
+        puts cookies[:username]
+    end
 end
