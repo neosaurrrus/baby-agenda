50e5b7d3ecb5aa478454fafed2d188580d81b342
neosaurrrus <lukiek@gmail.com>
2020-08-27 07:55:23 +0100

    before rethink of agenda system

--- baby_agenda_backend/app/models/user.rb
+++ baby_agenda_backend/app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
+    attr_reader :agenda
 
     has_many :activities, :class_name => "Activity"
     has_many :agenda_activities, :class_name => "Activity", :foreign_key => "agenda_activity_id"
+
+    def initialize_agenda
+        self.agenda = Array.new
+    end
+
+  
 end
